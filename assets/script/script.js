d2d76dc820b0f3113369a739808e6f69f1c5e9ad
Deborah Galasso <galassodeborah@outlook.com>
2023-06-05 13:02:14 +0100

    Add conversion formula from fahrenheit and kelvin

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,5 @@
 //declaration
-const convertFrom = parseFloat(document.querySelector('#insert-number').value);
+const convertFrom = parseFloat(document.querySelector('#insert-number').innerText);
 const fromUnit = document.querySelector('#degrees-from');
 const toUnit = document.querySelector('#degrees-to');
 const submitButton = document.querySelector('#convert')
@@ -8,11 +8,39 @@ const submitButton = document.querySelector('#convert')
 submitButton.addEventListener('click', convertTemperature)
 
 //functions
-function convertTemperature(){
+function convertTemperature(e){
+    e.preventDefault();
+
     let result;
+    //Conversion from Celsius 
     if (fromUnit === 'Celsius') {
         if (toUnit === 'Fahrenheit') {
             result = (convertFrom * 9/5) + 32;
+        } else if (toUnit === 'Kelvin') {
+            result = convertFrom + 273.15;
+        } else {
+            result = convertFrom;
+        } 
+    //Conversion from Fahrenheit
+        if (fromUnit === 'Fahrenheit') {
+            if (toUnit === 'Celsius') {
+            result = (convertFrom - 32) * 5/9;
+        } else if (toUnit === 'Kelvin') {
+            result = (convertFrom - 32) * 5/9 + 273.15;
+        } else {
+            result = convertFrom;
         }
     }
+    //Conversion from Kelvin
+        if (fromUnit === 'Kelvin') {
+            if (toUnit === 'Celsius') {
+                result = convertFrom - 273.15;
+            } else if (toUnit === 'Fahrenheit') {
+                result = (convertFrom - 273.15) * 9/5 + 32;
+            } else {
+                result = convertFrom;
+            } 
+        }
+    }   
+    document.getElementById('result').innerText = result;
 }

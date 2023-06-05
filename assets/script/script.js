4d4027714ff6bdf3b46965f45e2395974c2df178
Deborah Galasso <galassodeborah@outlook.com>
2023-06-05 12:33:09 +0100

    Set up conversion from celsius to fahrenheit

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,18 @@
 //declaration
-const convertFrom = document.querySelector('#insert-number');
+const convertFrom = parseFloat(document.querySelector('#insert-number').value);
 const fromUnit = document.querySelector('#degrees-from');
 const toUnit = document.querySelector('#degrees-to');
-let result = document.querySelector('#result');
-let submitButton = document.querySelector('#convert')
+const submitButton = document.querySelector('#convert')
 
 //event listner 
 submitButton.addEventListener('click', convertTemperature)
 
 //functions
 function convertTemperature(){
-
+    let result;
+    if (fromUnit === 'Celsius') {
+        if (toUnit === 'Fahrenheit') {
+            result = (convertFrom * 9/5) + 32;
+        }
+    }
 }

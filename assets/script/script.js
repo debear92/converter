3886fb228df815b80ac13cab2f0e8d9a9984da01
Deborah Galasso <galassodeborah@outlook.com>
2023-06-05 12:20:07 +0100

    Declare constant and variables in js file, create event listener

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,14 @@
+//declaration
 const convertFrom = document.querySelector('#insert-number');
+const fromUnit = document.querySelector('#degrees-from');
+const toUnit = document.querySelector('#degrees-to');
 let result = document.querySelector('#result');
 let submitButton = document.querySelector('#convert')
 
+//event listner 
 submitButton.addEventListener('click', convertTemperature)
 
+//functions
 function convertTemperature(){
 
 }

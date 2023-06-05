659cd532ebbd726d0e89defe7a91fabf6a9fc359
Deborah Galasso <galassodeborah@outlook.com>
2023-06-05 12:17:21 +0100

    Build HTML structure of the page

---
+++ assets/script/script.js
@@ -0,0 +1,9 @@
+const convertFrom = document.querySelector('#insert-number');
+let result = document.querySelector('#result');
+let submitButton = document.querySelector('#convert')
+
+submitButton.addEventListener('click', convertTemperature)
+
+function convertTemperature(){
+
+}
